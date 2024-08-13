d81bb724cddb46740e5fbb11b6ea6ca82cb15baf
Scott Rigg <srrigg3@gmail.com>
2024-08-13 13:42:19 -0600

    Building up the template

---
+++ capstone-webapp/src/Footer.js
@@ -0,0 +1,31 @@
+
+
+function Footer() {
+    return (
+        <>
+        <footer>
+        <img src='./Logo.png' alt="Little Lemon Logo"/>
+            <ul id="siteNav">
+                <li><a href='./#home'>Home</a></li>
+                <li><a href='./#about'>About</a></li>
+                <li><a href='./#menu'>Menu</a></li>
+                <li><a href='./#reservations'>Reservations</a></li>
+                <li><a href='./#order'>Order Online</a></li>
+                <li><a href='./#login'>Login</a></li>
+            </ul>
+            <ul id="contact">
+                <li>Contact Us</li>
+                <li>(555) 555-5555</li>
+                <li>littlelemonrestaurant@realemailaddress.com</li>
+            </ul>
+            <ul id="socials">
+                <li><a href='./#Facebook'>Facebook</a></li>
+                <li><a href='./#X'>X</a></li>
+                <li><a href='./#Instagram'>Instagram</a></li>
+            </ul>
+        </footer>
+        </>
+    );
+  };
+  
+  export default Footer;
---
+++ capstone-webapp/src/Main.js
@@ -0,0 +1,16 @@
+
+
+function Main() {
+    return (
+        <>
+        <main>
+            Main Reservation Section
+            <body>
+                Reservation dropdowns and popups go here
+            </body>
+        </main>
+        </>
+    );
+  };
+  
+  export default Main;
